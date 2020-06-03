0e548d33efe995f59b9a9559434e7129f008ffae
bence <borbelybence98@gmail.com>
2020-06-03 16:53:56 +0200

    single agent single manager

---
+++ provision.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Purpose - Script to add a user to Linux system including passsword
+# Author - Vivek Gite <www.cyberciti.biz> under GPL v2.0+
+# ------------------------------------------------------------------
+# Am i Root user?
+if [ $(id -u) -eq 0 ]; then
+	username="admin"
+	password="admin"
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		useradd -m -p "$pass" "$username"
+		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+fi
+
+## SSH
+
+sudo apt-get -y update
+mkdir /home/admin/.ssh
+cat /vagrant/*.pub >> /home/admin/.ssh/authorized_keys
+
+## SNMP
+
+sudo apt-get install -y snmpd nano
+
+sudo cp /vagrant/snmpd.conf /etc/snmp/snmpd.conf
+
